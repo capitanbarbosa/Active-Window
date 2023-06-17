0f28c4bcd9b0c98e2db375dbe74a5f572a73ef1b
Wiz <wizbarbosa@gmail.com>
2023-06-16 23:52:07 -0500

    window-switcherv1

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from cx_Freeze import setup, Executable
+import ctypes
 
-# Dependencies are automatically detected, but it might need
-# fine tuning.
-build_options = {'packages': [], 'excludes': []}
+def get_virtual_desktop_names():
+    user32 = ctypes.windll.user32
+    virtual_desktops = []
 
-import sys
-base = 'Win32GUI' if sys.platform=='win32' else None
+    def enum_windows_callback(hwnd, lParam):
+        title_length = user32.GetWindowTextLengthW(hwnd)
+        title_buffer = ctypes.create_unicode_buffer(title_length + 1)
+        user32.GetWindowTextW(hwnd, title_buffer, title_length + 1)
+        title = title_buffer.value
 
-executables = [
-    Executable('active_window.pyw', base=base)
-]
+        if title.startswith("Desktop"):
+            virtual_desktops.append(title)
 
-setup(name='Active Window',
-      version = '0.99',
-      description = 'Log of tasks connected via Notion API.',
-      options = {'build_exe': build_options},
-      executables = executables)
+        return True
+
+    user32.EnumWindows(ctypes.WNDENUMPROC(enum_windows_callback), 0)
+
+    return virtual_desktops
+
+virtual_desktops = get_virtual_desktop_names()
+print(virtual_desktops)
